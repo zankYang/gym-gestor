38e97772febc8c9d6d9f116f96e60eb90a4ed689
Alejandro Sanchez <alejandrozksj@gmail.com>
2026-03-05 23:52:51 -0600

    Refactor AccessTokenController: improve error handling and response structure in store and destroy methods. Replace error throws with appropriate response messages in Spanish for better user experience.

--- app/controllers/auth/access_token_controller.ts
+++ app/controllers/auth/access_token_controller.ts
@@ -4,30 +4,34 @@ import type { HttpContext } from '@adonisjs/core/http'
 import hash from '@adonisjs/core/services/hash'
 
 export default class AccessTokenController {
-  async store({ request, serialize }: HttpContext) {
+  async store({ request, response }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
 
     const user = await User.query().where('email', email).first()
     if (!user) {
-      throw new Error('User not found')
+      return response.status(401).send({
+        message: 'Usuario no encontrado',
+      })
     }
     const isPasswordValid = await hash.verify(user.password, password)
     if (!isPasswordValid) {
-      throw new Error('Invalid password')
+      return response.status(401).send({
+        message: 'Contraseña incorrecta',
+      })
     }
 
     const token = await User.accessTokens.create(user)
 
-    return serialize({
-      user,
-      token: token.value!.release(),
+    return response.status(200).send({
+      message: 'Conectado correctamente',
+      data: {
+        token: token.value!.release(),
+      },
     })
   }
 
-  async destroy({ auth }: HttpContext) {
+  async destroy({ auth, response }: HttpContext) {
     await auth.use('api').invalidateToken()
-    return {
-      message: 'Logged out successfully',
-    }
+    return response.status(200).send({ message: 'Desconectado correctamente' })
   }
 }
